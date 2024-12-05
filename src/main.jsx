552c8dbf0b1c7acc20083d5d50315d2301627419
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2024-12-05 17:37:01 +0900

    ・ルーティングを導入する。

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   //<React.StrictMode>
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
   //</React.StrictMode>
 );
